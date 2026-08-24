9786cef045322d6386f8fedfd573d4a871e46a6e
Kshitij9934 <kshitijdeo75@gmail.com>
2026-08-24 12:36:58 +0530

    Add files via upload

---
+++ kshitijdeo.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+// Declaration
+int age;
+float salary;
+char grade;
+bool ispassed;
+string name;
+
+//Intialization
+age = 20;
+salary = 45000.50;
+grade = 'A' ;
+ispassed = true;
+name = "kshitij";
+
+//Declaration + Initialization
+int rollNo = 101;
+string studentName = "Alice";
+
+cout << "age: " << age << endl;
+cout << "Salary: " << salary << endl;
+cout << "Passed: " << ispassed << endl;
+cout << "Name: " << name << endl;
+return 0;
+}
